c54cb868386b1ffc19a645ff4169786b2f4df35c
jpguille <jpguillen2008@gmail.com>
2026-05-18 22:12:09 -0300

    Update exercise_07_write_inventory.py

--- exercise_07_write_inventory.py
+++ exercise_07_write_inventory.py
@@ -28,4 +28,6 @@ def write_inventory(filename, inventory):
         # iron:7
         # wood:10
     """
-    pass  # Reemplazar con tu implementación
+    with open(filename, 'w') as archivo:
+        for item, cantidad in sorted(inventory.items()):
+            archivo.write(f"{item}:{cantidad}\n")
